1f06c8ddafb64d4f8a101df465a65ee8194e6f0b
Hossein Haghbin <haghbinh.gmail.com>
2018-09-13 14:48:59 +0430

    ِDeathrate dataset is documated.

---
+++ R/Deathrate.R
@@ -0,0 +1,15 @@
+#' The Age-specific death rates dataset.
+#'
+#'   The death rates for years 1921 to 2014 and age range 0 to 100.
+#'
+#' @name Deathrate
+#' @format A data frame with 10434 rows and 5 variables:
+#' \describe{
+#'   \item{Year}{The year}
+#'   \item{Age}{The age}
+#'   \item{Female}{The female death rate}
+#'   \item{Male}{The male death rate}
+#'   \item{Total}{The total death rate}
+#' }
+#' @source \url{http://www.mortality.org/}
+"Deathrate"
