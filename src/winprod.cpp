ff9cd4483d90a6b27522f9c06a12385fcd61e926
haghbinh <haghbinh@gmail.com>
2019-07-18 15:08:20 +0430

    Convertig winprod into cpp version

---
+++ src/winprod.cpp
@@ -0,0 +1,20 @@
+#include <RcppArmadillo.h>
+
+// w-inner product
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+//'@importFrom Rcpp sourceCpp
+// [[Rcpp::export]]
+double winprod(arma::mat x, arma::mat y, arma::vec w,  arma::mat G)
+{
+  int N = x.n_cols;
+  arma::vec out(N);
+  arma::vec xi(x.n_rows),yi(y.n_rows);
+  for(int i=0; i<N; i++ )
+  {
+    xi = x.col(i);
+    yi = y.col(i);
+    out(i) = arma::as_scalar(xi.t()*G*yi*w(i));
+  }
+  return(sum(out));
+}
