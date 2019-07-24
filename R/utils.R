f98bd5c660c4c92d122c66ba694bef7f216ffbd2
jtrinka <jordantrinka4@hotmail.com>
2019-07-24 13:03:08 -0500

    updating utils.R for new mfssa.R output

--- R/utils.R
+++ R/utils.R
@@ -45,57 +45,71 @@ V <- function(U, d, K, L, Y) {
 # right singular vecjtors for the multivariate case
 mV<-function(U,values,K,Y,A,p,L){
   sourceCpp("HpLinprod.cpp")
+  p=length(U[[1]])
+  K=N-L+1
   V <- matrix(nrow=K,ncol=length(values),data=0)
+  G <- list()
+  # get inner producjt matrices
+  for(i in 1:p){
+    
+    G[[i]] = inprod(Y[[i]]$basis,Y[[i]]$basis)
+    
+  }
   for(i in 1:length(values)){
     element <- U[[i]]
     u <- list()
     for(k in 1:K){
       x <- list()
       for(j in 1:p){
-        u[[j]] <- element[[j]]$coefs
-        x[[j]] <- lagvec_new(Y[[j]]$coefs,L,k)
+        
+        u[[j]]=element[[j]]$coefs
+        x[[j]]=lagvec_new(Y[[j]]$coefs,L,k)
+        
+        
       }
-      V[k,i] <- HpLinprod(u,x,A,p)/sqrt(values[i])
+      
+      V[k,i] <- HpLinprod(u,x,G,p)/sqrt(values[i])
+      
     }
   }
   return(V)
 }
 
 # Rank one approximajtion
-mfproj <- function(out, i, K, L, Y){
+mfproj <- function(U, i, K, L, Y){
   # defining pieces of multivariate rank one approximation
   sourceCpp("HpLinprod.cpp")
   u <- list()
-  pc_1 <- out$U[[1]]
+  pc_1 <- U[[1]]
   p <- length(pc_1)
   for(j in 1 : p){
-    element<-out$U[[i]]
+    element<-U[[i]]
     u[[j]]=element[[j]]$coefs
   }
   C = list()
-  A = list()
+  G = list()
   for(j in 1:p){
-
+    
     d=nrow(pc_1[[j]]$coefs)
     C[[j]]=array(NA, dim = c(d, K, L))
-    A[[j]] = inprod(Y[[j]]$basis,Y[[j]]$basis)
-
+    G[[j]] = inprod(Y[[j]]$basis,Y[[j]]$basis)
+    
   }
   # define HpL lag vector
   for(k in 1:K){
     x <- list()
     for(j in 1:p){
       x[[j]]=lagvec_new(Y[[j]]$coefs,L,k)
-
+      
     }
     #build operajtor
     for(j in 1:p){
       C_jx <- C[[j]]
-      C_jx[, k, ] <- HpLinprod(x,u,A,p)*u[[j]]
+      C_jx[, k, ] <- HpLinprod(x,u,G,p)*u[[j]]
       C[[j]] <- C_jx
-
+      
     }
-
+    
   }
   return(C)
 }
