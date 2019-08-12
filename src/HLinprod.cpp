9775eeafd439d878ff66d691daeb9ea765191b79
haghbinh <haghbinh@gmail.com>
2019-08-12 16:25:53 +0430

    fixing scale problem

--- src/HLinprod.cpp
+++ src/HLinprod.cpp
@@ -7,13 +7,13 @@ using namespace Rcpp;
 double HLinprod(arma::mat x, arma::mat y, arma::mat G)
 {
   int N = x.n_cols;
-  double s;
+  double s = 0;
   arma::vec xi(x.n_rows),yi(y.n_rows);
   for(int i=0; i<N; i++ )
   {
     xi = x.col(i);
     yi = y.col(i);
-    s += arma::as_scalar(xi.t()*G*yi);
+    s += arma::as_scalar(xi.t()*yi);
   }
   return(s);
 }
--- src/HpLinprod.cpp
+++ src/HpLinprod.cpp
@@ -12,19 +12,20 @@ double HpLinprod(std::vector<arma::mat> X, std::vector<arma::mat> Y, std::vector
   double s = 0;
   arma::mat x;
   arma::mat y;
-  arma::mat g;
+  //arma::mat g;
   for(int j=0; j<p; j++){
-	
+
 	x=X[j];
 	y=Y[j];
-	g=G[j];
+	//g=G[j];
 	int N = x.n_cols;
 	arma::vec xi(x.n_rows),yi(y.n_rows);
 	for(int i=0; i<N; i++)
 	{
 		xi = x.col(i);
 		yi = y.col(i);
-		s += arma::as_scalar(xi.t()*g*yi);
+		//s += arma::as_scalar(xi.t()*g*yi);
+		s += arma::as_scalar(xi.t()*yi);
 	}
   }
   return(s);
