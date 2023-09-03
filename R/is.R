7bb994aca017c83486e10082d5ca93f2c585119b
Hossein <36113130+haghbinh@users.noreply.github.com>
2023-09-03 22:02:26 +0330

    new

--- R/is.R
+++ R/is.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #' Check if an object is of class 'funts'
 #'
 #' @param obj The object to check.
@@ -6,13 +5,4 @@
 #' @export
 is.funts <- function(obj) {
   inherits(obj, "funts")
-=======
-#' Check if an object is of class 'funfts'
-#'
-#' @param obj The object to check.
-#' @return TRUE if the object is of class 'funfts', FALSE otherwise.
-#' @export
-is.funfts <- function(obj) {
-  inherits(obj, "funfts")
->>>>>>> 7551633c78ae9dba6b403d65fa72dcd5691d31de
 }
